b214af7f02b9e796c0411f8117ef2d469d4d6bb8
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-18 19:40:19 -0400

    Implemented to return the return statement with its respective min/max value

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -10,9 +10,6 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Maximum Value = " + myMaxIntValue);
-        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
-        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
 
-        int myMaxIntTest = 2_147_483_647;
     }
 }
