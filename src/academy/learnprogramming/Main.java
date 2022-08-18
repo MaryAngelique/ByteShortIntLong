91c2a946b4ca8931edd05d87c9d15537c0303c0b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-08-18 18:54:56 -0400

    Implemented variables to print out minimum and maximum values

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -6,5 +6,9 @@ public class Main {
 
         int myValue = 10000;
 
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
     }
 }
